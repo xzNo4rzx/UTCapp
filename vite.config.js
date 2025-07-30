64ba738bf2656014a5ae3d99d8d21dbdc4fd5321
xzNo4rzx <xzno4rzx@gmail.com>
2025-07-30 17:32:34 +0200

    🚀 Fix start bug UTC app (connect-history-api-fallback)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import history from 'connect-history-api-fallback';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
-// Remplace cette URL par l'URL de ton API en ligne si tu veux proxy en dev
-const API_BASE = "https://api.exemple.com";
+// Remplace cette URL par l'URL de ton API si tu veux proxy en dev
+const API_BASE = "https://github.com/xzNo4rzx/ai-signal-api/tree/main";
 
 export default defineConfig({
   plugins: [react()],
@@ -16,6 +18,11 @@ export default defineConfig({
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
+    },
+    middlewareMode: 'html',
+    setupMiddlewares(middlewares) {
+      middlewares.unshift(history());
+      return middlewares;
     },
   },
   build: {
